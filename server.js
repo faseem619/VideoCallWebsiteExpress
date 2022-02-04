e87a4b4f253a8c6891c46673026301f66cc705d4
faseem619 <faseem619@gmail.com>
2022-02-04 18:26:19 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params.roomId });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+  });
+});
+
+server.listen(3000);
