f01b881b23b9a2bc29b9faa5876af7a6bb9d33de
faseem619 <faseem619@gmail.com>
2022-02-10 13:31:31 +0530

    changed stun and turn servers

--- public/js/room.js
+++ public/js/room.js
@@ -59,8 +59,6 @@ const peer = new Peer(undefined, {
   config: {
     iceServers: [
       { urls: "stun:stun.l.google.com:19302" },
-      { urls: "stun:stun1.l.google.com:19302" },
-      { urls: "stun:stun2.l.google.com:19302" },
       {
         urls: "turn:numb.viagenie.ca",
         credential: "muazkh",
